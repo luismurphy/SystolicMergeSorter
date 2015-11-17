26763779f37044e6e54e44d5ebfedb0d1db354df
andresaleman <andres.aleman@upr.edu>
2015-11-17 15:14:09 -0400

    Project with Elaborated Design

--- Workspace/processingModule.vhd
+++ Workspace/processingModule.vhd
@@ -1,5 +1,10 @@
 -- ENTITY DECLARATION: module input and output ports of a device
 -- port names, port sizes, directions
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity processing_module is port( 
 	IL : in std_logic_vector(7 downto 0); 
 	IR : in std_logic_vector(7 downto 0);
@@ -12,17 +17,18 @@ end processing_module;
 
 -- ARCHITECTURE BLOCK
 architecture Behavioral of processing_module is
-    signal regS : std_logic_vector(7 downto 0):='0';
-    signal regB : std_logic_vector(7 downto 0):='0';
+    signal regS : std_logic_vector(7 downto 0):="00000000";
+    signal regB : std_logic_vector(7 downto 0):="00000000";
 
 -- FUNCTIONAL CODE: module functionality and implementation
 begin
-	process(rising_edge(clk))
+	process(clk) --change process(rising_edge(clk)) to process(clk)
 	begin 
+	if(rising_edge(clk)) then  --added this if
 		if(regS < IL and IL <= regB and regB <= IR) then
 			regS <= IL;
 		elsif (regB < IL and regB <= IR) then
-			regS <= RB; regB <= IL;
+			regS <= regB; regB <= IL; --change Rb for regB in regS<=regB
 		elsif (IL <= regS and regS <= IR and IR < regB) then
 			regB <= IR;
 		elsif (IL <= regS and IR < regS) then
@@ -32,6 +38,7 @@ begin
 		elsif(regS <= IR and IR < IL and IR < regB) then
 			regS <= IR;regB <= IL;
 		end if;
+	end if;
 		RS <= regS;
 		RB <= regB;
 	end process;
